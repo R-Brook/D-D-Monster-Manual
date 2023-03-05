b979c74698e4fc97bb771077f66d2c413d8ff6fa
R Brook <rlbrook@gmail.com>
2023-03-05 16:40:46 +0000

    [feature] Add filter context.

---
+++ context/types.ts
@@ -0,0 +1,27 @@
+export type SelectedFiltersState = {
+  monsterSize: string;
+  monsterType: string;
+  monsterAC: string;
+};
+
+/*
+export type Pagination = {
+  resultsTotal: number;
+  filteredList: any;
+  currentPage: number;
+  entriesPerPage: number;
+  numberOfPages: number;
+  shownItems: any;
+};
+*/
+
+export type Action =
+  | { type: "setMonsterSize"; payload: string }
+  | { type: "clearMonsterSize" }
+  | { type: "setMonsterType"; payload: string }
+  | { type: "clearMonsterType" }
+  | { type: "setMonsterAC"; payload: string }
+  | { type: "clearMonsterAC" }
+  | { type: "clearAllFilters" };
+
+export type FilterDispatch = (action: Action) => void;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { Header } from "components/header";
+import { SelectedFiltersProvider } from "context/filterContext";
 import type { AppProps } from "next/app";
 import "../css/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <SelectedFiltersProvider>
+        <Header />
+        <Component {...pageProps} />
+      </SelectedFiltersProvider>
     </>
   );
 }
