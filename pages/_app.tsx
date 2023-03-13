230e76f10f629696b29733bd4c91c3ffab8e9ec6
R Brook <rlbrook@gmail.com>
2023-03-13 22:45:35 +0000

    [feature] Remove filters context.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Header } from "components/header";
-import { SelectedFiltersProvider } from "context/filters/filterContext";
 import { PaginationProvider } from "context/pagination/paginationContext";
 import type { AppProps } from "next/app";
 import "../css/styles.css";
@@ -7,12 +6,10 @@ import "../css/styles.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SelectedFiltersProvider>
-        <PaginationProvider>
-          <Header />
-          <Component {...pageProps} />
-        </PaginationProvider>
-      </SelectedFiltersProvider>
+      <PaginationProvider>
+        <Header />
+        <Component {...pageProps} />
+      </PaginationProvider>
     </>
   );
 }
