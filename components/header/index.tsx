537490b796b0ffd340f5cba9b69b383ca54ae298
R Brook <rlbrook@gmail.com>
2023-06-26 22:40:13 +0100

    [#23] Small header and footer styling updates.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const Header = ({}) => {
   return (
     <div className={styles.layout}>
-      <div className="container">
+      <div className={styles.container}>
         <Link href={"/"} className={styles.link}>
           <h1 className={styles.title}>D&D 5e Monster Manual</h1>
         </Link>
